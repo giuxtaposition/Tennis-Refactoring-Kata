892912848019dbe39c890f028782919916d9bd69
giuxtaposition <yg97.cs@gmail.com>
2021-10-11 09:36:31 +0200

    refactored TennisGame1 making more readable public methods

--- src/TennisGame1.ts
+++ src/TennisGame1.ts
@@ -11,19 +11,86 @@ export class TennisGame1 implements TennisGame {
     this.Player2Name = Player2Name;
   }
 
-  wonPoint(playerName: string): void {
-    playerName === "player1" ? this.player1Score++ : this.player2Score++;
-  }
-
   getScore(): string {
-    if (this.player1Score === this.player2Score) {
+    if (this.isDraw()) {
       return this.drawResult();
     }
-    if (this.player1Score >= 4 || this.player2Score >= 4) {
-      return this.winOrAdvantageResult();
+    if (this.isAdvantage()) {
+      return this.advantageResult();
+    }
+    if (this.isWin()) {
+      return this.winResult();
     }
 
     return this.ongoingResult();
+  }
+
+  wonPoint(playerName: string): void {
+    this.isPlayer1(playerName)
+      ? this.incrementScorePlayer1()
+      : this.incrementScorePlayer2();
+  }
+
+  isPlayer1(playerName) {
+    return playerName === "player1";
+  }
+
+  incrementScorePlayer2() {
+    this.player2Score++;
+  }
+
+  incrementScorePlayer1() {
+    this.player1Score++;
+  }
+
+  isDraw() {
+    return this.player1Score === this.player2Score;
+  }
+
+  drawResult() {
+    switch (this.player1Score) {
+      case 0:
+        return "Love-All";
+      case 1:
+        return "Fifteen-All";
+      case 2:
+        return "Thirty-All";
+      default:
+        return "Deuce";
+    }
+  }
+
+  isAdvantage() {
+    return (
+      this.hasPlayerReachedFourPoints() &&
+      (this.getScoreDifference() === 1 || this.getScoreDifference() === -1)
+    );
+  }
+
+  isWin() {
+    return (
+      this.hasPlayerReachedFourPoints() &&
+      (this.getScoreDifference() >= 2 || this.getScoreDifference() <= -2)
+    );
+  }
+
+  hasPlayerReachedFourPoints() {
+    return this.player1Score >= 4 || this.player2Score >= 4;
+  }
+
+  getScoreDifference() {
+    return this.player1Score - this.player2Score;
+  }
+
+  advantageResult() {
+    return "Advantage " + this.playerWithHighestScore();
+  }
+  winResult() {
+    return "Win for " + this.playerWithHighestScore();
+  }
+
+  playerWithHighestScore() {
+    return this.player1Score > this.player2Score ? "player1" : "player2";
   }
 
   ongoingResult() {
@@ -45,26 +112,5 @@ export class TennisGame1 implements TennisGame {
       case 3:
         return "Forty";
     }
-  }
-
-  drawResult() {
-    switch (this.player1Score) {
-      case 0:
-        return "Love-All";
-      case 1:
-        return "Fifteen-All";
-      case 2:
-        return "Thirty-All";
-      default:
-        return "Deuce";
-    }
-  }
-
-  winOrAdvantageResult() {
-    const minusResult: number = this.player1Score - this.player2Score;
-    if (minusResult === 1) return "Advantage player1";
-    else if (minusResult === -1) return "Advantage player2";
-    else if (minusResult >= 2) return "Win for player1";
-    else return "Win for player2";
   }
 }
