b327f312e820437b2b880826706083cb37a13b9a
giuxtaposition <yg97.cs@gmail.com>
2021-10-20 10:06:25 +0200

    renamed attributes to be more readable

--- src/TennisGame1.ts
+++ src/TennisGame1.ts
@@ -1,127 +1,130 @@
-import { TennisGame } from "./TennisGame";
+import { TennisGame } from './TennisGame'
 
 export class TennisGame1 implements TennisGame {
-  private Player1: Player;
-  private Player2: Player;
+    private firstPlayer: Player
+    private secondPlayer: Player
 
-  constructor(Player1Name: string, Player2Name: string) {
-    this.Player1 = new Player(Player1Name);
-    this.Player2 = new Player(Player2Name);
-  }
-
-  getScore(): string {
-    if (this.isDraw()) {
-      return this.drawResult();
-    }
-    if (this.isAdvantage()) {
-      return this.advantageResult();
-    }
-    if (this.isWin()) {
-      return this.winResult();
+    constructor(firstPlayerName: string, secondPlayerName: string) {
+        this.firstPlayer = new Player(firstPlayerName)
+        this.secondPlayer = new Player(secondPlayerName)
     }
 
-    return this.ongoingResult();
-  }
+    getScore(): string {
+        if (this.isDraw()) {
+            return this.drawResult()
+        }
+        if (this.isAdvantage()) {
+            return this.advantageResult()
+        }
+        if (this.isWin()) {
+            return this.winResult()
+        }
 
-  wonPoint(playerName: string): void {
-    this.isPlayer1(playerName)
-      ? this.Player1.incrementScore()
-      : this.Player2.incrementScore();
-  }
-
-  isPlayer1(playerName) {
-    return playerName === "player1";
-  }
-
-  isDraw() {
-    return this.Player1.score === this.Player2.score;
-  }
-
-  drawResult() {
-    switch (this.Player1.score) {
-      case 0:
-        return "Love-All";
-      case 1:
-        return "Fifteen-All";
-      case 2:
-        return "Thirty-All";
-      default:
-        return "Deuce";
+        return this.ongoingResult()
     }
-  }
 
-  isAdvantage() {
-    return (
-      this.hasPlayerReachedFourPoints() &&
-      (this.getScoreDifference() === 1 || this.getScoreDifference() === -1)
-    );
-  }
-
-  isWin() {
-    return (
-      this.hasPlayerReachedFourPoints() &&
-      (this.getScoreDifference() >= 2 || this.getScoreDifference() <= -2)
-    );
-  }
-
-  hasPlayerReachedFourPoints() {
-    return this.Player1.score >= 4 || this.Player2.score >= 4;
-  }
-
-  getScoreDifference() {
-    return this.Player1.score - this.Player2.score;
-  }
-
-  advantageResult() {
-    return "Advantage " + this.playerWithHighestScore();
-  }
-  winResult() {
-    return "Win for " + this.playerWithHighestScore();
-  }
-
-  playerWithHighestScore() {
-    return this.Player1.score > this.Player2.score ? "player1" : "player2";
-  }
-
-  ongoingResult() {
-    return (
-      this.ongoingResultTerms(this.Player1.score) +
-      "-" +
-      this.ongoingResultTerms(this.Player2.score)
-    );
-  }
-
-  ongoingResultTerms(score) {
-    switch (score) {
-      case 0:
-        return "Love";
-      case 1:
-        return "Fifteen";
-      case 2:
-        return "Thirty";
-      case 3:
-        return "Forty";
+    wonPoint(playerName: string): void {
+        this.isFirstPlayer(playerName)
+            ? this.firstPlayer.incrementScore()
+            : this.secondPlayer.incrementScore()
     }
-  }
+
+    isFirstPlayer(playerName) {
+        return playerName === 'player1'
+    }
+
+    isDraw() {
+        return this.firstPlayer.score === this.secondPlayer.score
+    }
+
+    drawResult() {
+        switch (this.firstPlayer.score) {
+            case 0:
+                return 'Love-All'
+            case 1:
+                return 'Fifteen-All'
+            case 2:
+                return 'Thirty-All'
+            default:
+                return 'Deuce'
+        }
+    }
+
+    isAdvantage() {
+        return (
+            this.hasPlayerReachedFourPoints() &&
+            (this.getScoreDifference() === 1 ||
+                this.getScoreDifference() === -1)
+        )
+    }
+
+    isWin() {
+        return (
+            this.hasPlayerReachedFourPoints() &&
+            (this.getScoreDifference() >= 2 || this.getScoreDifference() <= -2)
+        )
+    }
+
+    hasPlayerReachedFourPoints() {
+        return this.firstPlayer.score >= 4 || this.secondPlayer.score >= 4
+    }
+
+    getScoreDifference() {
+        return this.firstPlayer.score - this.secondPlayer.score
+    }
+
+    advantageResult() {
+        return 'Advantage ' + this.playerWithHighestScore()
+    }
+    winResult() {
+        return 'Win for ' + this.playerWithHighestScore()
+    }
+
+    playerWithHighestScore() {
+        return this.firstPlayer.score > this.secondPlayer.score
+            ? 'player1'
+            : 'player2'
+    }
+
+    ongoingResult() {
+        return (
+            this.ongoingResultTerms(this.firstPlayer.score) +
+            '-' +
+            this.ongoingResultTerms(this.secondPlayer.score)
+        )
+    }
+
+    ongoingResultTerms(score) {
+        switch (score) {
+            case 0:
+                return 'Love'
+            case 1:
+                return 'Fifteen'
+            case 2:
+                return 'Thirty'
+            case 3:
+                return 'Forty'
+        }
+    }
 }
 
 class Player {
-  private _score: number = 0;
-  private _name: string;
+    private _score: number = 0
+    private _name: string
 
-  constructor(name) {
-    this._name = name;
-  }
+    constructor(name) {
+        this._name = name
+    }
 
-  get name() {
-    return this._name;
-  }
+    get name() {
+        return this._name
+    }
 
-  get score() {
-    return this._score;
-  }
+    get score() {
+        return this._score
+    }
 
-  incrementScore() {
-    this._score++;
-  }
+    incrementScore() {
+        this._score++
+    }
 }
